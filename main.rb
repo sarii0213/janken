8212e6a41d18858047fddb3f86e7ed964680ef00
sarii0213 <anjing.singka@gmail.com>
2022-06-15 13:48:13 +0900

    最低限要件を満たしたjankenプログラム実装

---
+++ main.rb
@@ -0,0 +1,61 @@
+class Janken
+  def initialize
+    @results = {}
+    @results.default = 0
+  end
+
+  def start
+    puts "何本勝負？(press 1 or 3 or 5)"
+    @number = gets.chomp 
+    puts "#{@number}本勝負を選びました"
+    main
+  end
+
+  def main
+    @number.to_i.times.with_index do |n, i|  
+      puts "\n"
+      puts "#{i+1}本目"
+      puts "じゃんけん...(press g or c or p)"
+      play
+    end
+    show_result
+  end
+
+  def play
+    player = gets.chomp
+    cpu = ['g', 'c', 'p'].sample
+    janken = {'g' => 'グー', 'p' => 'パー', 'c' => 'チョキ'}
+    puts "CPU...#{janken[cpu]}"
+    puts "あなた...#{janken[player]}"
+    judge(cpu, player)
+  end
+
+  def judge(cpu, player)
+    if cpu == player
+      puts 'あいこで...(press g or c or p)'
+      play
+    elsif cpu == 'p' && player == 'g' || cpu == 'g' && player == 'c' || cpu == 'c' && player == 'p'
+      @results[:lose] += 1
+      puts '負け！'
+      puts "#{@results[:win]}勝#{@results[:lose]}敗"
+    else
+      @results[:win] += 1
+      puts '勝ち！'
+      puts "#{@results[:win]}勝#{@results[:lose]}敗"
+    end
+  end
+  
+  def show_result
+    if @results[:lose] < @results[:win]
+      result = "勝ち"
+    else
+      result = "負け"
+    end
+    puts "\n"
+    puts '結果' 
+    puts "#{@results[:win]}勝#{@results[:lose]}敗であなたの#{result}"
+  end
+end
+    
+janken = Janken.new
+janken.start
